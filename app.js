116ce9202eef7b0ed6208663ce14e4893219937a
sagsilver <50857685+sagsilver@users.noreply.github.com>
2020-01-30 00:01:58 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+
+    var articlesImagen=document.getElementsByClassName('imagen-article');
+
+console.log(articlesImagen.src);
+
+// var imagenMuestra=document.querySelector('.imagen-muestra');
+// imagenMuestra.src=articlesImagen;
